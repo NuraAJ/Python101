3564cea2549b6767caf9e0466f84bebf840429ec
NuraAJ <nura.aljaafari@kaust.edu.sa>
2018-08-12 16:05:24 +0300

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -24,6 +24,17 @@ def fib(x, mem):
 def fib_mem(x):
     mem = [None] * (x+1)
     return fib(x, mem)
+
+#Using bottom up approach
+def fib_buttom_up(x):
+    if (x == 1) or (x == 2):
+        return 1
+    bottom_up = [None] * (x + 1)
+    bottom_up[1] = 1
+    bottom_up[2] = 1
+    for i in range(3, x+1):
+        bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
+    return bottom_up[x]
     
 #print results and compare time 
 start_time = time.time()
@@ -34,3 +45,8 @@ print("Recursion took: --- %s seconds ---" % (time.time() - start_time))
 start_time = time.time()
 fib_mem(11)
 print("DP took: --- %s seconds ---" % (time.time() - start_time))
+
+start_time = time.time()
+print(fib_bottom_up(11))
+print("BU took: --- %s seconds ---" % (time.time() - start_time))
+
